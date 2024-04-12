af5a724ef41f42fd2300b1126c49253528d182cb
Sala K 405 <salak405@salasfr.itm.edu.co>
2024-04-12 16:38:20 -0500

    jugador init

---
+++ jugador.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+class Jugador {
+	public:
+		int id;
+		int puntos;
+		int guardianes_restantes;
+		int exploradores;
+		string nombre;
+
+		Jugador(int id, string nombre);
+
+		int mover_ficha(int jugador_id, int movimiento1, int movimient2);
+		void forzar_turno_perdido();
+};
+
+
+c
