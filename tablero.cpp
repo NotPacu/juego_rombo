d249e53e2a354d8ec03c47e8ce8ca9fabb29f582
SkchupDePana <jerogeimer14@gmail.com>
2024-04-15 01:56:11 -0500

    implementacion del tablero, mover y descartar movimientos

--- tablero.cpp
+++ tablero.cpp
@@ -13,129 +13,206 @@ si es negativo no está confirmado
 #define ro "\033[31m"
 #define rs "\033[0m"
 
-class Tablero{
-    private:
-        static const int tab = 11;
-        int casillas(int x);
-        
+class Tablero
+{
+private:
+    static const int tab = 11;
 
-    public:
-        int tamanos[tab]; 
-        int numero_jugadores;
-        std::vector<int*> tablero;
-        std::vector<int*> tablero_bac;
-        
-        Tablero(int numero_jugadores);
-        int* retornar_columna(int columna);
-        int mover_ficha(int jugador_id,int movimiento_1,int movimiento_2);
-        int traducir_codigo(int ficha);
-        bool tiene_movimientos(int* movimientos,int jugador_id);
-        void perder_turno(int jugador_id);
-        void cerrar_turno(int jugaodr_id);
-        void imprimir_tablero();
+    int casiilas_bloqueadas[44];
+    int movimientos_cap[11];
+    int movimientos_exp[11];
+
+    int mover_ficha_c(int jugador_id, int movimiento, int ficha_movil);
+
+public:
+    int acciones[12];
+    int size[tab];
+    int numero_jugadores;
+    std::vector<int *> tablero;
+    std::vector<int *> tablero_bac;
+
+    Tablero(int numero_jugadores);
+
+    int traducir_codigo(int ficha);
+    bool tiene_movimientos(int *movimientos, int jugador_id);
+    void perder_turno(int jugador_id);
+    void cerrar_turno(int jugaodr_id);
+    void imprimir_tablero();
+
+    int *movimientos_capitan(int jugador_id);
+    int *movimientos_explorador(int jugador_id);
+    int mover_explorador(int jugador_id, int columna);
+    // no me hago responsable si le ponen un numero que no está permitido
+    // para eso le hice funciones que descartan los movimientos ilegales
+    // att: jero
 };
 
-
-int Tablero::traducir_codigo(int cod){
+int Tablero::traducir_codigo(int cod)
+{
     cod = abs(cod);
-   
-    int id = cod%4;
-    if(id == 0) id = numero_jugadores;
+
+    int id = cod % 4;
+    if (id == 0)
+        id = numero_jugadores;
     return id;
-
 }
-
-void Tablero::imprimir_tablero(){
-    for (int i = 0;i<tab;i++){
-        for (int t = 0; t < tamanos[i];t++){
-            std::cout<<tablero[i][t]<<"/";
-        }
-        std::cout<<"\n";
+int *Tablero::movimientos_capitan(int jugador_id)
+{
+    for (int col = 0; col < 11; col++)
+    {
+        movimientos_cap[col] = -1;
     }
-    std::cout<<ro<<"tablero copia\n";
-    for (int i = 0;i<tab;i++){
-        for (int t = 0; t < tamanos[i];t++){
-            std::cout<<tablero_bac[i][t]<<"/";
-        }
-        std::cout<<"\n";
-    }
-    std::cout<<"ESTO NO ES PARA GRAFICAR SOLO PARA AYUDAR A ENCONTRAR BUGS O DEPURAR , NO USEN ESTO EN LA VERSION FINAL"<<rs<<"\n";
-}
-
-int Tablero::casillas(int x){
-    return (x*2+1);
-}
-
-int* Tablero::retornar_columna(int columna){
-    //retorna el puntero de cada columna
-    //el indice empieza desde 0
-    return tablero[columna];
-}
-
-
-
-int Tablero::mover_ficha(int jugador_id,int movimiento_1,int movimiento_2){
-    //retorna la cantidad de puntos coronados
-    //movimiento 1 de un explorador normal
-    //movimiento 2 del capitan
-    int coronados = 0;
-    int* c1 = retornar_columna(movimiento_1-2);
-    int* c2 = retornar_columna(movimiento_2-2);
-
-    for (int i = tamanos[movimiento_1-2];i >= 0 ;i++){
-        if(traducir_codigo(c1[i]) == jugador_id){
-            if (i >= tamanos[movimiento_1-2]){
-                coronados++;
-                
+    for (int col = 0; col < 11; col++)
+    {
+        for (int row = 0; row < 11; row++)
+        {
+            if (tablero[col][row] == jugador_id + 4)
+            {
+                movimientos_cap[col] = col;
+                return movimientos_cap;
             }
         }
     }
 
-
+    for (int i = 0; i < 11; i++)
+    {
+        if (casiilas_bloqueadas[i + (jugador_id - 1) * 11] == 1)
+        {
+            movimientos_cap[i] = -1;
+        }
+        else
+        {
+            movimientos_cap[i] = i;
+        }
+    }
+    return movimientos_cap;
 }
-
-
-Tablero::Tablero(int numero_jugadores_){
-    numero_jugadores = numero_jugadores_;
-
-    for (int i = 0;i <= 5;i++){
-        tamanos[i] = casillas(i+1);
+int Tablero::mover_explorador(int jugador_id, int columna)
+{
+    for (int fila = size[columna] - 1; fila >= 0; fila--)
+    {
+        /*
+        std::cout<<traducir_codigo(tablero[columna][fila])<<std::endl;
+        std::cout<<fila<<std::endl;
+        std::cout<<tablero[columna][fila]<<std::endl;
+        */
+        if (traducir_codigo(tablero[columna][fila]) == jugador_id)
+        {
+            for (int i = fila + 1; i <= size[columna] + 1; i++)
+            {
+                if (i >= size[columna])
+                {
+                    tablero[columna][fila] = 0;
+                    return 1;
+                }
+                if (tablero[columna][i] == 0)
+                {
+                    tablero[columna][fila] = 0;
+                    tablero[columna][i] = jugador_id;
+                    return 0;
+                }
+            }
+        }
     }
-    for (int i = 6;i <= 10;i++){
-        tamanos[i] = casillas(tab-i);
-    }
 
-    for (int i = 0;i<tab;i++){
-        tablero.push_back(new int[tamanos[i]]);
-        tablero_bac.push_back(new int[tamanos[i]]);
+    if (casiilas_bloqueadas[columna + (jugador_id - 1) * 11] == 1)
+    {
+        return -1;
     }
-
-    for (int i = 0;i<tab;i++){
-        for (int t = 0; t < tamanos[i];t++){
-            tablero[i][t] = 0;
-            tablero_bac[i][t] = 0;
+    for (int fila = 0; fila <= size[columna]; fila++)
+    {
+        if (fila >= size[columna]){
+            return 1;
+        }
+        if (tablero[columna][fila] == 0)
+        {
+            tablero[columna][fila] = jugador_id;
+            return 0;
         }
 
     }
+    return -1;
+}
+int *Tablero::movimientos_explorador(int jugador_id)
+{
+    for (int col = 0; col < 11; col++)
+    {
+        movimientos_exp[col] = -1;
+    }
+    return movimientos_exp;
+}
+Tablero::Tablero(int numero_jugadores)
+{
+    this->numero_jugadores = numero_jugadores;
 
-    /*
-    0-0-0-
-    0-0-0-0-0-
-    0-0-0-0-0-0-0-
-    0-0-0-0-0-0-0-0-0-
-    0-0-0-0-0-0-0-0-0-0-0-
-    0-0-0-0-0-0-0-0-0-0-0-0-0-
-    0-0-0-0-0-0-0-0-0-0-0-
-    0-0-0-0-0-0-0-0-0-
-    0-0-0-0-0-0-0-
-    0-0-0-0-0-
-    0-0-0-
-    */
+    for (int i = 0; i <= 5; i++)
+    {
+        size[i] = (i + 1) * 2 + 1;
+    }
+    for (int i = 6; i <= 10; i++)
+    {
+        size[i] = ((tab - i) * 2 + 1);
+    }
+
+    for (int i = 0; i < tab; i++)
+    {
+        tablero.push_back(new int[size[i]]);
+        tablero_bac.push_back(new int[size[i]]);
+    }
+
+    for (int i = 0; i < tab; i++)
+    {
+        for (int t = 0; t < size[i]; t++)
+        {
+            tablero[i][t] = 0;
+            tablero_bac[i][t] = 0;
+        }
+    }
+
+    for (int i = 0; i < 44; i++)
+    {
+        casiilas_bloqueadas[i] = 0;
+    }
 }
 
-int main(){
+void Tablero::imprimir_tablero()
+{
+    std::cout << "\n";
+    for (int i = 0; i < tab; i++)
+    {
+        for (int t = 0; t < size[i]; t++)
+        {
+            std::cout << tablero[i][t] << "  ";
+        }
+        std::cout << "\n";
+    }
+    /*
+    std::cout << ro << "tablero copia\n";
+    for (int i = 0; i < tab; i++)
+    {
+        for (int t = 0; t < size[i]; t++)
+        {
+            std::cout << tablero_bac[i][t] << "/";
+        }
+        std::cout << "\n";
+    }
+    */
+    std::cout << "ESTO NO ES PARA GRAFICAR SOLO PARA AYUDAR A ENCONTRAR BUGS O DEPURAR , NO USEN ESTO EN LA VERSION FINAL" << rs << "\n";
+}
+int main()
+{
     Tablero t(4);
+
+    t.tablero[1][4] = 0;
+    t.tablero[1][3] = 10;
+    t.tablero[1][2] = 1;
+    t.tablero[1][1] = 10;
+    t.tablero[1][0] = 10;
+
+
+    std::cout << t.mover_explorador(1, 1) << std::endl;
+
     t.imprimir_tablero();
 
-    return 0;
+    return 2;
 }
