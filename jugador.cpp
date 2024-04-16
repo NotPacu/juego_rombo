3303cdd554d4866f2bf1501098d7f23136749ed6
SkchupDePana <jerogeimer14@gmail.com>
2024-04-15 20:22:39 -0500

    moviendo cositas a jugador

--- jugador.cpp
+++ jugador.cpp
@@ -4,15 +4,15 @@
 class Jugador {
 	public:
 		int id;
-		int puntos;
-		int guardianes_restantes;
-		int exploradores;
+		int puntos = 0;
+		int guardianes_restantes = 10;
+		int exploradores = 5;
 		std::string nombre;
 
 		Jugador(int id, std::string nombre);
 
-		int mover_ficha(int jugador_id, int movimiento1, int movimient2);
-		void forzar_turno_perdido();
+		//int mover_ficha(int jugador_id, int movimiento1, int movimient2);
+		//void forzar_turno_perdido();
 };
 
 Jugador::Jugador(int id_, std::string nombre_){
